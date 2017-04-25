28ef756a96b4353554b328c41a68e78a63bc5151
greengie <green.gie1234@gmail.com>
2017-04-26 05:02:44 +0700

    add more select items

--- src/component/selector_kmean.js
+++ src/component/selector_kmean.js
@@ -3,13 +3,20 @@ import Select from 'react-select'
 import 'react-select/dist/react-select.css'
 
 var data = [
-  { value: 'gdp', label: 'GDP' },
-  { value: 'popdensity', label: 'Population Density' },
-  { value: 'income_per_person', label: 'Income per person' },
-  { value: 'life_expectancy', label: 'Life expectancy at birth' },
+  { value: 'gdp', label: 'GDP (current US$)' },
+  { value: 'popdensity', label: 'Population Density (per sq. km.)' },
+  { value: 'income_per_person', label: 'Income per person (fixed PPP$)' },
+  { value: 'life_expectancy', label: 'Life expectancy at birth (years)' },
   { value: 'total_population', label: 'Total Population' },
-  { value: 'expense_on_health', label: 'Total Expenditure On Health' },
-  { value: 'work_hour_per_week', label: 'Working hours per week' }
+  { value: 'expense_on_health', label: 'Total Expenditure On Health (PPP int. $)' },
+  { value: 'work_hour_per_week', label: 'Working hours per week (hours)' },
+  { value: "tax_revenue", label: 'Tax Revenue (% of GDP)'},
+  { value: "inflation", label: 'Inflation, GDP deflator (annual %)'},
+  {value: "infant_mortality", label: 'Infant mortality (rate per 1,000 births)'},
+  {value: "internet_user", label: 'Internet User (people who use)'},
+  {value: "girl_boy_ratio_education", label: 'Ratio of girls to boys in primary and secondary education (%)'},
+  {value: "energy_use",	label: 'Energe use, total (toe)'},
+  {value: "agricultural_land", label: 'Agricultural land (% of land area)'}
 ];
 
 function handleChange(e) {
